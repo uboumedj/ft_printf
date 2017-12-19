0c928046c12f650d076c9bf95a9dbed198f10656
Unknown <uboumedj@student.42.fr>
2017-12-19 19:26:56 +0100

    small changes

--- ft_printflags.c
+++ ft_printflags.c
@@ -6,7 +6,7 @@
 /*   By: uboumedj <uboumedj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/17 01:30:54 by uboumedj          #+#    #+#             */
-/*   Updated: 2017/12/19 18:35:11 by uboumedj         ###   ########.fr       */
+/*   Updated: 2017/12/19 19:26:20 by uboumedj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ size_t	print_flag(va_list *vlist, t_printf *handler, int mod)
 {
 	if (handler->spec == '%')
 		return(print_char('%', handler));
-	else if (handler->spec == 'd' || handler->spec == 'i' || handler->spec == 'D')
+	else if (handler->spec == 'd' || handler->spec == 'i')
 		return (do_nb(vlist, handler, mod));
 	else if (handler->spec == 'u' || handler->spec == 'U' )
 		return (do_unb(vlist, handler, mod));
+	else if (handler->spec == 'o' ||handler->spec == 'O')
+		return (do_onb(vlist, handler, mod));
 	else if (handler->spec == 'c' || handler->spec == 'C')
 		return (do_char(vlist, handler, mod));
 	else if (handler->spec == 's' || handler->spec == 'S')
