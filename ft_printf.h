22b4cf83432b02c2ff2aad81d8ea7d1c1773627d
Unknown <uboumedj@student.42.fr>
2017-12-21 05:18:32 +0100

    small mistake

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: uboumedj <uboumedj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 10:37:31 by uboumedj          #+#    #+#             */
-/*   Updated: 2017/12/21 04:57:51 by uboumedj         ###   ########.fr       */
+/*   Updated: 2017/12/21 05:18:24 by uboumedj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,13 @@ size_t    print_hexnb(unsigned long long int res, t_printf *handler);
 size_t    do_bnb(va_list *vlist, int mod, t_printf *handler);
 size_t    print_bnb(unsigned long long int res, t_printf *handler);
 
-size_t    do_ptr(va_list *vlist, t_printf *handler, int mod);
+size_t    do_ptr(va_list *vlist, int mod, t_printf *handler);
 size_t		print_ptr(size_t ptr, t_printf *handler);
 
-size_t    do_char(va_list *vlist, t_printf *handler, int mod);
+size_t    do_char(va_list *vlist, int mod, t_printf *handler);
 size_t    print_char(wchar_t c, t_printf *handler);
 
-size_t    do_str(va_list *vlist, t_printf *handler, int mod);
+size_t    do_str(va_list *vlist, int mod, t_printf *handler);
 size_t    print_str(char *str, t_printf *handler);
 
 #endif
